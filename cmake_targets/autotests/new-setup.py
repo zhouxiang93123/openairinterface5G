9e3d1d58f1c2de79cb81faedc895db1340f9f7d7
Cedric Roux <cedric.roux@eurecom.fr>
2016-12-15 10:31:31 +0100

    let's revisit the test setup

---
+++ cmake_targets/autotests/new-setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import os
+
+print os.environ.get('OAI_USER')
+print os.environ.get('OAI_PASS')
+print os.environ.get('OAI_TEST_CASE_GROUP')
+print os.environ.get('MACHINELIST')
+print os.environ.get('MACHINELISTGENERIC')
+print os.environ.get('RESULT_DIR')
+print os.environ.get('NRUNS_LTE_SOFTMODEM')
